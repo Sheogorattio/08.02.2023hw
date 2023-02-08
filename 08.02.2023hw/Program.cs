d1d8c11f18636ca2468262f9762c04e4469c71cf
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-08 23:36:41 +0200

    Add project files.

---
+++ 08.02.2023hw/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorSpace;
+
+namespace _08._02._2023hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector A = new Vector(1,2,3);
+            Vector B = new Vector(1,2,3);
+            Console.Write($"A coord:");
+            A.Print();
+            Console.WriteLine("A length:{0:0.00}", A.Length);
+            Console.Write($"B coord:");
+            B.Print();
+            Console.WriteLine("B length:{0:0.00}", B.Length);
+            Console.WriteLine($"A==B:{A.IsEqual(ref B)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 08.02.2023hw/Vector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorSpace
+{
+    public class Vector
+    {
+        public double ValX { get; set; }
+        public double ValY { get; set; }
+        public double ValZ { get; set; }
+        public double Length { get => Math.Sqrt(Math.Pow(ValX, 2) + Math.Pow(ValY, 2) + Math.Pow(ValZ, 2)); }
+
+        public Vector()
+        {
+            ValX = 0;
+            ValY = 0;
+            ValZ = 0;
+        }
+        public Vector(double valX, double valY, double valZ)
+        {
+            ValX = valX;
+            ValY = valY;
+            ValZ = valZ;
+        }
+        public Vector(ref Vector obj)
+        {
+            ValX = obj.ValX;
+            ValY = obj.ValY;
+            ValZ = obj.ValZ;
+        }
+        public void Init()
+        {
+            Console.WriteLine("Enter X:");
+            ValX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y:");
+            ValY = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Z:");
+            ValZ = Convert.ToDouble(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine($"({ValX} ; {ValY} ; {ValZ})");
+        }
+        public void IncreaseBy(double val)
+        {
+            ValX += val;
+            ValY += val;
+            ValZ += val;
+        }
+        public void DecreaseBy(double val)
+        {
+            ValX-= val;
+            ValY-= val;
+            ValZ-= val;
+        }
+        public Vector Sum(ref Vector obj)
+        {
+            return new Vector(ValX+obj.ValX, ValY+obj.ValY, ValZ+obj.ValZ);
+        }
+
+        public Vector Diff(ref Vector obj)
+        {
+            return new Vector(ValX-obj.ValX, ValY-obj.ValY, ValZ-obj.ValZ);
+        }
+
+        public double Prod(ref Vector obj)
+        {
+            return ValX*obj.ValX+ValY*obj.ValY+ValZ*obj.ValZ;
+        }
+
+        public bool IsEqual(ref Vector obj)
+        {
+            return (Length == obj.Length) && ((ValX % obj.ValX == 0 && ValY % obj.ValY == 0 && ValZ % obj.ValZ == 0) || (obj.ValZ % ValZ == 0 && obj.ValY % ValY == 0 && obj.ValX % ValX == 0));
+        }
+    }
+}
